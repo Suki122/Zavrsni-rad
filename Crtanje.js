91c5bc0aa273846625684a23e28d39f70618284a
Suki122 <a.sucur2@gmail.com>
2026-06-16 18:43:40 +0200

    Dodan izbornik na pocetku, potrebno ugasiti zvuk ovaca i dodati intro glazbu za izbornik

--- Crtanje.js
+++ Crtanje.js
@@ -6,6 +6,9 @@ let travaKvadrat;
 let ograda;
 let ograde=[];
 let zvukOvce;
+let stanje="izbornik";
+let gumbIgraj;
+
 //osiguranje da se slike i  zvukovi ucitaju prvi
 function preload(){
     slikaOvce=loadImage("resursi/sheep_walk.png");
@@ -16,32 +19,52 @@ function preload(){
 
 function setup(){
     createCanvas(800,600);
+    gumbIgraj=createButton("IGRAJ");
+    gumbIgraj.class('play-gumb');
+    gumbIgraj.position(width/2-50,height/2+50);
+    gumbIgraj.mousePressed(pokreniIgru);
+    
     //oznacavamo koji tile iz pozadine zelimo 
     travaKvadrat=pozadina.get(0,0,16,16);
+
     //spawn Boida odreden svojom grupom
-    // let centri=[
-    //     createVector(200,200),
-    //     createVector(600,200),
-    //     createVector(400,400)
-    // ];
     for(let i=0;i<30;i++){
-        // let g=floor(random(0,3));
-        // let centar=centri[g];
         let centar=createVector(200,200);
         let b=new Boid(centar.x+random(-30,30),centar.y+random(-30,30),slikaOvce,zvukOvce);
-        // b.grupa=g;
         flock.push(b);
     }
     for(let j=0;j<5;j++){
-        //let z=new Zid(random(0,800),random(0,600),random(0,150),random(0,150));
         let o2=new Ograda(random(0,800),random(0,600),5,"v",ograda);
-        //zidovi.push(z);
         ograde.push(o2);
     }
 }
-    
 
-function draw(){
+function draw() {
+    crtaIgru(); 
+    
+    if (stanje === "izbornik") {
+        push();
+        fill(255, 255, 255, 200); // Bijela s prozirnošću (200 od 255)
+        rect(150, 100, 500, 400, 20); // Pravokutnik izbornika
+        
+        fill(255, 152, 0); // Narančasta za naslov
+        textAlign(CENTER);
+        textSize(48);
+        text("MOJA FARMA", width/2, height/2 - 50);
+        
+        fill(255, 235, 59); // Žuta za podnaslov
+        textSize(24);
+        text("Pomozi ovcama da izbjegnu ogradu!", width/2, height/2 - 10);
+        pop();
+    } else {
+        gumbIgraj.hide();
+    }
+}
+
+function crtaIgru() {
+    // Tvoja postojeća logika za crtanje trave i ovaca ide ovdje
+    background(100, 200, 100); 
+    // ... ostatak crtanja boida ...
     noSmooth();  //da bi p5.js prikazao pozadinu ostro, inace bi bila mutna
     for(let x=0;x<width;x+=16*2){ //ide po x-u
         for(let y=0;y<height;y+=16*2){ //ide po y
@@ -60,4 +83,17 @@ function draw(){
         boid.kretanje(flock,zidovi,ograde);
         boid.prikazi();
     }
+}
+
+function prikaziIzbornik() {
+    background(50);
+    textAlign(CENTER);
+    fill(255);
+    textSize(50);
+    text("MOJA OVCA IGRA", width/2, height/2 - 50);
+}
+
+function pokreniIgru() {
+    stanje = "igra";
+    gumbIgraj.hide(); // Sakrij gumb kad igra krene
 }
